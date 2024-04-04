9521d7b8d0dd1dad0b17372e0e3b2232104817da
gywls20 <122339761+gywls20@users.noreply.github.com>
2024-04-04 17:31:46 +0900

    프로그래머스 핸드폰 번호 가리기

---
+++ src/programmers/HidePhoneNumber.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        char[] arr = phone_number.toCharArray();
+        
+        for(int i=0; i<arr.length-4; i++){
+            arr[i]='*';
+            //System.out.print(arr[i]+" ");
+        }
+        for(int i=0; i<arr.length; i++){
+            answer+=arr[i];
+        }
+        return answer;
+    }
+}
