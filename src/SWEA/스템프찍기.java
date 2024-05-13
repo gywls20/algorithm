496df3e79bc94deab69e90b2c8fb2477eee46ed4
gywls20 <koj858@naver.com>
2024-05-14 00:50:38 +0900

    swea: 스탬프찍기

--- src/SWEA/스템프찍기.java
+++ src/SWEA/스템프찍기.java
@@ -1,2 +1,23 @@
-package SWEA;public class 스템프찍기 {
+package SWEA;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class 스템프찍기
+{
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            System.out.print("#");
+        }
+    }
 }
