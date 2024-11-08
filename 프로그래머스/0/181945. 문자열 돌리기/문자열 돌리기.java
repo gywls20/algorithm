91b1c93796cfbc092a3b1b09f26da80523eafa80
gywls20 <122339761+gywls20@users.noreply.github.com>
2024-11-09 00:58:11 +0900

    [level 0] Title: 문자열 돌리기, Time: 156.34 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String[] s = a.split("");
+        
+        for(int i=0; i<s.length; i++){
+            System.out.println(s[i]);
+        }
+        
+    }
+}
